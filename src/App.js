156e928c639de29ee35ddafbdcfa8d0e93a32664
khyogeun_@naver.com <khyogeun_@naver.com>
2023-04-26 17:59:06 +0900

    fix: Omission semicolon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "pages/Home";
 import "styles/global.css";
 import Detail from "pages/Detail";
 import Upload from "pages/Upload";
-import LogIn from "pages/Login"
+import LogIn from "pages/Login";
 import SignUp from "pages/SignUp";
 
 function App() {
